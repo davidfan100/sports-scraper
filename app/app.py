a1c583260bd51f2105a861eaf922854e49734b90
David Fan <davidf3@illioni.edu>
2019-08-22 21:41:19 -0500

    reorganized files

---
+++ app/app.py
@@ -0,0 +1,7 @@
+'''
+- create a flask app or even a django app (depending on how much time I have left)
+- create a single page application to show that I can use it to create a website to 
+- select players and show stats and compare with other players
+- my new feature is that I can compare using weight and height, which isn't common in other webistes
+- grab data from sql datatable
+'''
---
+++ src/baseball_collector.py
@@ -0,0 +1,6 @@
+'''
+- First import the mysql and beautiful soup (web scrapping) libraries
+- Create a function to collect the information from baseball reference using beautiful soup
+- Then create an sql database and create tables based on player's name alphabetically
+- add height/weight to the database to be able to filter and organize on the webapp
+'''
