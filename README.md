442dda5c7cc22acde67f329f1f1eaf96eb307fd0
David Fan <davidf3@illioni.edu>
2019-08-23 19:29:06 -0500

    started implementing the web scrapping feature

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # sports-scraper
-A web scrapper that will obtain and sort data on MLB or NBA players based on an attribute or filter, such as height, weight, and primary throwing arm
+A web scrapper that will obtain and sort data on current MLB players based on an attribute or filter, such as height, weight, and primary throwing arm
 
 eg: displaying MLB players who have a thrown over 200 IP that are sorted
 by ascending height
+
+TODO: Extend to NBA players as well
--- src/baseball_collector.py
+++ src/baseball_collector.py
@@ -1,6 +1,33 @@
 '''
-- First import the mysql and beautiful soup (web scrapping) libraries
+- First import the sqlite and beautiful soup (web scrapping) libraries
 - Create a function to collect the information from baseball reference using beautiful soup
 - Then create an sql database and create tables based on player's name alphabetically
 - add height/weight to the database to be able to filter and organize on the webapp
 '''
+
+import sqlite3
+import requests
+import logging
+from bs4 import BeautifulSoup
+def scrape_data_website():
+    start_url = 'https://www.baseball-reference.com/players/a'
+    base_url = 'https://www.baseball-reference.com'
+    response = requests.get(start_url)
+    
+    soup_handler = BeautifulSoup(response.text, "html.parser")
+    temp = soup_handler.find_all('b') # bold means the player are active
+
+    urls = []
+    for i in temp:
+        if i.a == None:
+            continue
+        
+        urls.append(base_url + i.a.get('href'))
+        
+    
+    print(urls)
+
+
+
+if __name__ == '__main__':
+    scrape_data_website()
